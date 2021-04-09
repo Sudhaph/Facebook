db91bdf4a69fa48724d3bf85905c58a24cf727e1
sudhaph <sudha.h.1992@gmail.com>
2021-04-09 12:29:18 +0530

    greens

---
+++ src/test/java/org/com/test2.java
@@ -0,0 +1,20 @@
+package org.com;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void tc2() {
+		System.out.println("TC2");
+
+	}
+	@Test
+	public void tc3() {
+		System.out.println("TC3");
+	}
+
+		@Test
+		public void tc1() {
+			System.out.println("TC1");
+		}
+}
